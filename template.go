3bca90044c5e94dcca38a6ea1cae4becebae557a
Andrew Clissold <andrewclissold@gmail.com>
2014-06-19 17:26:11 -0400

    Update Go template

--- template.go
+++ template.go
@@ -1,39 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"strings"
 )
 
 func main() {
-	lines := readLines()
-	for _, line := range lines {
-		fmt.Println(line)
+	f, _ := os.Open(os.Args[1])
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
-}
-
-// readLines does the work bufio's scanner API would do, but CodeEval doesn't have Go 1.1 yet.
-func readLines() (lines []string) {
-	// Avoid a panic accessing os.Args[1]
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Usage: ./XXX <file>") // XXX
-		os.Exit(1)
-	}
-
-	// Read data from a file given as a command line argument
-	data, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	// Parse the lines of the file from data
-	lines = strings.Split(string(data), "\n")
-
-	// Remove the empty string after the final \n
-	lines = lines[:len(lines)-1]
-
-	return
 }
