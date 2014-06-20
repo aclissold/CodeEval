03096c3ccf42fed152047b506512cc015be1dacd
Andrew Clissold <andrewclissold@gmail.com>
2014-06-19 22:56:54 -0400

    Add missing problem descriptions

--- problem092.go
+++ problem092.go
@@ -1,3 +1,27 @@
+/*
+Problem 92: Penultimate Word
+
+CHALLENGE DESCRIPTION:
+
+Write a program which finds the next-to-last word in a string.
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename. Input example is the
+following
+
+    some line with text
+    another line
+    Each line has more than one word.
+
+OUTPUT SAMPLE:
+
+Print the next-to-last word in the following way.
+
+	with
+	another
+*/
+
 package main
 
 import (
--- problem115.go
+++ problem115.go
@@ -1,3 +1,26 @@
+/*
+Problem 115: Mixed Content
+
+CHALLENGE DESCRIPTION:
+
+You have a string of words and digits divided by comma. Write a program which separates words with
+digits. You shouldn't change the order elements.
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename. Input example is the
+following
+
+    8,33,21,0,16,50,37,0,melon,7,apricot,peach,pineapple,17,21
+    24,13,14,43,41
+
+OUTPUT SAMPLE:
+
+    melon,apricot,peach,pineapple|8,33,21,0,16,50,37,0,7,17,21
+    24,13,14,43,41
+
+As you can see you need to output the same input string if it has words only or digits only.
+*/
 package main
 
 import (
