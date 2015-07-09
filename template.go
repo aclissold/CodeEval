7af5f8b954ba69f878638fa2f67ffe1e0a29dc21
Andrew Clissold <andrewclissold@gmail.com>
2015-07-08 21:50:14 -0400

    Update Go template

--- template.go
+++ template.go
@@ -3,12 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	f, _ := os.Open(os.Args[1])
-	scanner := bufio.NewScanner(f)
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
